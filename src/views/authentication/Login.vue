57df963ab8e5f2cd82fe7e457b341d68c0533b4c
Luis G <lsgrrd@gmail.com>
2021-06-19 18:18:28 -0500

    se implemento el login de la v1

--- src/views/authentication/Login.vue
+++ src/views/authentication/Login.vue
@@ -211,18 +211,18 @@
 
 <script>
 /* eslint-disable global-require */
+import { mapActions } from 'vuex'
 import { ValidationProvider, ValidationObserver } from 'vee-validate'
 import VuexyLogo from '@core/layouts/components/Logo.vue'
 import {
   BRow, BCol, BLink, BFormGroup, BFormInput, BInputGroupAppend, BInputGroup, BFormCheckbox, BCardText, BCardTitle, BImg, BForm, BButton, BAlert, VBTooltip,
 } from 'bootstrap-vue'
-import useJwt from '@/auth/jwt/useJwt'
 import { required, email } from '@validations'
 import { togglePasswordVisibility } from '@core/mixins/ui/forms'
 import store from '@/store/index'
-import { getHomeRouteForLoggedInUser } from '@/auth/utils'
+// import { getHomeRouteForLoggedInUser } from '@/auth/utils'
 import { $themeConfig } from '@themeConfig'
-import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
+// import ToastificationContent from '@core/components/toastification/ToastificationContent.vue'
 
 export default {
   setup() {
@@ -280,37 +280,16 @@ export default {
     },
   },
   methods: {
+    ...mapActions('auth', ['signIn']),
     login() {
       this.$refs.loginForm.validate().then(success => {
         if (success) {
-          useJwt.login({
-            email: this.userEmail,
-            password: this.password,
-          })
-            .then(response => {
-              const { userData } = response.data
-              useJwt.setToken(response.data.accessToken)
-              useJwt.setRefreshToken(response.data.refreshToken)
-              localStorage.setItem('userData', JSON.stringify(userData))
-              this.$ability.update(userData.ability)
-
-              // ? This is just for demo purpose. Don't think CASL is role based in this case, we used role in if condition just for ease
-              this.$router.replace(getHomeRouteForLoggedInUser(userData.role))
-                .then(() => {
-                  this.$toast({
-                    component: ToastificationContent,
-                    position: 'top-right',
-                    props: {
-                      title: `Welcome ${userData.fullName || userData.username}`,
-                      icon: 'CoffeeIcon',
-                      variant: 'success',
-                      text: `You have successfully logged in as ${userData.role}. Now you can start to explore!`,
-                    },
-                  })
-                })
+          this.signIn({ email: this.userEmail, password: this.password })
+            .then(() => {
+              debugger
             })
-            .catch(error => {
-              this.$refs.loginForm.setErrors(error.response.data.error)
+            .catch(() => {
+              debugger
             })
         }
       })
