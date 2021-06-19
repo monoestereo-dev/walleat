062a2d47955e84075992ef2bd1f40141c0001ede
Luis G <lsgrrd@gmail.com>
2021-06-19 18:18:13 -0500

    baseURL

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  // baseURL: 'https://some-domain.com/api/',
+  baseURL: 'https://api.mywalleat.com',
   // timeout: 1000,
   // headers: {'X-Custom-Header': 'foobar'}
 })
