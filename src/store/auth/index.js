f9ba744085def6b8c4f45cea9de854484d742413
Luis G <lsgrrd@gmail.com>
2021-06-19 18:18:05 -0500

    module ready to login

--- src/store/auth/index.js
+++ src/store/auth/index.js
@@ -38,6 +38,7 @@ const actions = {
           context.commit('setCurrentUser', response.data)
           localStorage.setItem('currentUser', JSON.stringify(response.data))
           resolve(response.data)
+          router.push('/')
         })
         .catch(error => {
           reject(error)
