e1bb34c26d2495eefc50d1252e5b08d316ab70e0
Luis G <lsgrrd@gmail.com>
2021-06-20 20:27:29 -0500

    nuevos componentes

---
+++ src/views/user/users-view/UserViewEstablishmentCard.vue
@@ -0,0 +1,69 @@
+<template>
+  <b-card
+    no-body
+    class="border-primary"
+  >
+    <b-card-header class="d-flex justify-content-between align-items-center pt-75 pb-25">
+      <h5 class="mb-0">
+        Establecimiento
+      </h5>
+      <b-badge variant="light-primary">
+        Basic
+      </b-badge>
+    </b-card-header>
+
+    <b-card-body>
+      <div class="d-flex align-items-center justify-content-center flex-column p-1">
+        <h1
+          v-for="establishment in userData.scoped_roles.filter(x => { return x.role_resource_type === 'Establishment' })"
+          :key="`establishment-${establishment.id}`"
+        >
+          {{ establishment.role_resource_name }}
+        </h1>
+        <h3
+          v-for="establishment in userData.scoped_roles.filter(x => { return x.role_resource_type === 'Store' })"
+          :key="`establishment-${establishment.id}`"
+        >
+          {{ establishment.role_resource_name }}
+        </h3>
+      </div>
+      <b-button
+        v-ripple.400="'rgba(255, 255, 255, 0.15)'"
+        variant="primary"
+        block
+      >
+        Agregar credito
+      </b-button>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script>
+import {
+  BCard, BCardHeader, BCardBody, BBadge, BButton,
+} from 'bootstrap-vue'
+import Ripple from 'vue-ripple-directive'
+
+export default {
+  directives: {
+    Ripple,
+  },
+  components: {
+    BCard,
+    BCardHeader,
+    BCardBody,
+    BBadge,
+    BButton,
+  },
+  props: {
+    userData: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- src/views/user/users-view/UserViewUserInfoCard.vue
+++ src/views/user/users-view/UserViewUserInfoCard.vue
@@ -12,8 +12,8 @@
         <!-- User Avatar & Action Buttons -->
         <div class="d-flex justify-content-start">
           <b-avatar
-            :src="userData.avatar"
-            :text="avatarText(userData.fullName)"
+            :src="`${apiUrl}${userData.logo}`"
+            :text="avatarText(userData.name)"
             :variant="`light-${resolveUserRoleVariant(userData.role)}`"
             size="104px"
             rounded
@@ -21,7 +21,7 @@
           <div class="d-flex flex-column ml-1">
             <div class="mb-1">
               <h4 class="mb-0">
-                {{ userData.fullName }}
+                {{ userData.name }}
               </h4>
               <span class="card-text">{{ userData.email }}</span>
             </div>
@@ -42,44 +42,6 @@
           </div>
         </div>
 
-        <!-- User Stats -->
-        <div class="d-flex align-items-center mt-2">
-          <div class="d-flex align-items-center mr-2">
-            <b-avatar
-              variant="light-primary"
-              rounded
-            >
-              <feather-icon
-                icon="DollarSignIcon"
-                size="18"
-              />
-            </b-avatar>
-            <div class="ml-1">
-              <h5 class="mb-0">
-                23.3k
-              </h5>
-              <small>Monthly Sales</small>
-            </div>
-          </div>
-
-          <div class="d-flex align-items-center">
-            <b-avatar
-              variant="light-success"
-              rounded
-            >
-              <feather-icon
-                icon="TrendingUpIcon"
-                size="18"
-              />
-            </b-avatar>
-            <div class="ml-1">
-              <h5 class="mb-0">
-                $99.87k
-              </h5>
-              <small>Annual Profit</small>
-            </div>
-          </div>
-        </div>
       </b-col>
 
       <!-- Right Col: Table -->
@@ -91,25 +53,13 @@
           <tr>
             <th class="pb-50">
               <feather-icon
-                icon="UserIcon"
-                class="mr-75"
-              />
-              <span class="font-weight-bold">Username</span>
-            </th>
-            <td class="pb-50">
-              {{ userData.username }}
-            </td>
-          </tr>
-          <tr>
-            <th class="pb-50">
-              <feather-icon
                 icon="CheckIcon"
                 class="mr-75"
               />
               <span class="font-weight-bold">Status</span>
             </th>
             <td class="pb-50 text-capitalize">
-              {{ userData.status }}
+              {{ userData.active_status }}
             </td>
           </tr>
           <tr>
@@ -121,31 +71,19 @@
               <span class="font-weight-bold">Role</span>
             </th>
             <td class="pb-50 text-capitalize">
-              {{ userData.role }}
+              {{ userData.role_name }}
             </td>
           </tr>
-          <tr>
-            <th class="pb-50">
-              <feather-icon
-                icon="FlagIcon"
-                class="mr-75"
-              />
-              <span class="font-weight-bold">Country</span>
-            </th>
-            <td class="pb-50">
-              {{ userData.country }}
-            </td>
-          </tr>
-          <tr>
+          <tr v-if="userData.customer">
             <th>
               <feather-icon
                 icon="PhoneIcon"
                 class="mr-75"
               />
-              <span class="font-weight-bold">Contact</span>
+              <span class="font-weight-bold">Phone</span>
             </th>
             <td>
-              {{ userData.contact }}
+              {{ userData.customer.cel_number }}
             </td>
           </tr>
         </table>
@@ -159,6 +97,7 @@ import {
   BCard, BButton, BAvatar, BRow, BCol,
 } from 'bootstrap-vue'
 import { avatarText } from '@core/utils/filter'
+import { mapGetters } from 'vuex'
 import useUsersList from '../users-list/useUsersList'
 
 export default {
@@ -170,6 +109,9 @@ export default {
       type: Object,
       required: true,
     },
+  },
+  computed: {
+    ...mapGetters(['apiUrl']),
   },
   setup() {
     const { resolveUserRoleVariant } = useUsersList()
