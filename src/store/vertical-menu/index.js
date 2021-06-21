c9b6cde4de69114ea61136791b474cf68efacff8
Luis G <lsgrrd@gmail.com>
2021-06-20 20:27:00 -0500

    new getter

--- src/store/vertical-menu/index.js
+++ src/store/vertical-menu/index.js
@@ -5,7 +5,11 @@ export default {
   state: {
     isVerticalMenuCollapsed: $themeConfig.layout.menu.isCollapsed,
   },
-  getters: {},
+  getters: {
+    isVerticalMenuCollapsed(state) {
+      return state.isVerticalMenuCollapsed
+    },
+  },
   mutations: {
     UPDATE_VERTICAL_MENU_COLLAPSED(state, val) {
       state.isVerticalMenuCollapsed = val
