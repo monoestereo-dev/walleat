a6da94b50989c67c6343287ab1f8faa16ddac99f
Luis G <lsgrrd@gmail.com>
2021-06-21 23:14:11 -0500

    Menu para mobile que se muestra en la parte inferior de la pantalla

---
+++ src/@core/components/MenuBottomMobile.vue
@@ -0,0 +1,63 @@
+<template>
+  <div
+    id="menu-bottom-menu"
+    class="d-flex justify-content-around align-items-center menu-shadow"
+  >
+    <feather-icon
+      size="24"
+      icon="UserIcon"
+    />
+    <div
+      v-ripple
+      class="qr-icon text-light"
+    >
+      <feather-icon
+        size="24"
+        icon="CpuIcon"
+      />
+    </div>
+
+    <feather-icon
+      size="24"
+      icon="DollarSignIcon"
+    />
+  </div>
+</template>
+
+<script>
+import Ripple from 'vue-ripple-directive'
+
+export default {
+  directives: {
+    Ripple,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+// Color palettes
+@import '~@core/scss/base/core/colors/palette-variables.scss';
+
+#menu-bottom-menu {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100vw;
+  height: 50px;
+  background: #283046;
+  box-shadow: 0 4px 24px 0 rgba(#000, 0.75);
+  z-index: 10000;
+}
+.qr-icon {
+  $size: 55px;
+  display: flex;
+  justify-content: center;
+  box-shadow: 0 4px 24px 0 rgba(#000, 0.5);
+  align-items: center;
+  background: $warning;
+  margin-bottom: 2rem;
+  width: $size;
+  height: $size;
+  border-radius: 2rem;
+}
+</style>
--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,28 +1,32 @@
 <template>
-  <p class="clearfix mb-0">
-    <span class="float-md-left d-block d-md-inline-block mt-25">
-      COPYRIGHT  © {{ new Date().getFullYear() }}
-      <b-link
-        class="ml-25"
-        href="https://1.envato.market/pixinvent_portfolio"
-        target="_blank"
-      >{{ appName }}</b-link>
-      <span class="d-none d-sm-inline-block">, All rights Reserved</span>
-    </span>
+  <div>
+    <p class="clearfix mb-0 d-none d-sm-block">
+      <span class="float-md-left d-block d-md-inline-block mt-25">
+        COPYRIGHT  © {{ new Date().getFullYear() }}
+        <b-link
+          class="ml-25"
+          href="https://mywalleat.com"
+          target="_blank"
+        >{{ appName }}</b-link>
+        <span class="d-none d-sm-inline-block">, All rights Reserved</span>
+      </span>
 
-    <span class="float-md-right d-none d-md-block">Hand-crafted &amp; Made with
-      <feather-icon
-        icon="HeartIcon"
-        size="21"
-        class="text-danger stroke-current"
-      />
-    </span>
-  </p>
+      <span class="float-md-right d-none d-md-block">Hand-crafted &amp; Made with
+        <feather-icon
+          icon="HeartIcon"
+          size="21"
+          class="text-danger stroke-current"
+        />
+      </span>
+    </p>
+    <menu-bottom-mobile class="d-block d-sm-none" />
+  </div>
 </template>
 
 <script>
 import { $themeConfig } from '@themeConfig'
 import { BLink } from 'bootstrap-vue'
+import MenuBottomMobile from '@core/components/MenuBottomMobile.vue'
 
 export default {
   setup() {
@@ -33,6 +37,7 @@ export default {
   },
   components: {
     BLink,
+    MenuBottomMobile,
   },
 }
 </script>
