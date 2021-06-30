6b39ae585cbfc0497f6456348965f70edf8e004e
Luis G <lsgrrd@gmail.com>
2021-06-29 23:49:31 -0500

    quite un string de mas

--- src/@core/layouts/components/app-navbar/components/UserDropdown.vue
+++ src/@core/layouts/components/app-navbar/components/UserDropdown.vue
@@ -7,7 +7,7 @@
     <template #button-content>
       <div class="d-sm-flex d-none user-nav">
         <p class="user-name font-weight-bolder mb-0">
-          {{ userData.name || userData.email || 'hol'}}
+          {{ userData.name || userData.email }}
         </p>
         <span class="user-status">{{ userData.role }}</span>
       </div>
