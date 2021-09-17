a3338fb3f93fe9fbc83f3ba3ef088bfbbc482e05
Luis G <lsgrrd@gmail.com>
2021-09-16 19:07:26 -0500

    store users

---
+++ src/views/stores/StoreUsers.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <user-list />
+  </div>
+</template>
+
+<script>
+import UserList from '@/views/user/users-list/UsersList.vue'
+
+export default {
+  components: {
+    UserList,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
